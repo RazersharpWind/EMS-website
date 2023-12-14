07d19814c2783e09246a5bc8c807eb93e1775528
Abdullah M. Aref <abdullah.m.aref@gmail.com>
2023-12-15 00:45:52 +0400

    created a Client View folder

--- Event Management System Web/wwwroot/js/newEventOutScope.js
+++ Event Management System Web/wwwroot/js/newEventOutScope.js
@@ -1,18 +1,18 @@
 const newEventButton2 = document.getElementById("dialog-button")
 
-    console.log(window.location.href.split("/"), window.location.href.split("/").indexOf("EMS"))
+console.log(window.location.href.split("/"), window.location.href.split("/").indexOf("Admin"))
 newEventButton2.addEventListener('click', (e) => {
     console.log(e.target);
     if (window.location.href.split("/").indexOf("Events") == -1) {
-        if (window.location.href.split("/").indexOf("EMS") == -1) { window.location.href = `EMS/Events?fromIndex=true`; }
-        else { window.location.href = `Events?fromIndex=true`; }
+        if (window.location.href.split("/").indexOf("Admin") == -1) { window.location.href = `Admin/Events?fromIndex=true`; }
+        else { window.location.href = `Admin?fromIndex=true`; }
     }
 
 })
 
-if (window.location.href != "EMS/Events") {
+if (window.location.href != "Admin/Events") {
     window.addEventListener("load", (e) => {
-        window.history.replaceState({}, "EMS/Events?fromIndex=true", document.querySelector("title").getAttribute("data-href"))
+        window.history.replaceState({}, "Admin/Events?fromIndex=true", document.querySelector("title").getAttribute("data-href"))
     })
 }
 
