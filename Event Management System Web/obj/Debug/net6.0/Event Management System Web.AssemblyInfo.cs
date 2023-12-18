42db5cec4408aa3276b5a47f15227bae819e2d28
Abdullah M. Aref <abdullah.m.aref@gmail.com>
2023-12-18 12:21:04 +0400

    fixed layout issues

--- Event Management System Web/obj/Debug/net6.0/Event Management System Web.AssemblyInfo.cs
+++ Event Management System Web/obj/Debug/net6.0/Event Management System Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event Management System Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c25b87919491dbfff1288c8339294815c249a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event Management System Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event Management System Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
