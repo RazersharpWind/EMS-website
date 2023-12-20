0271a8cce4d337f0dda0f66f93518f6063bcd14d
Abdullah M. Aref <abdullah.m.aref@gmail.com>
2023-12-20 15:03:07 +0400

    fixed all issues in both Manager and Client view

--- Event Management System Web/wwwroot/js/Client/viewEvent.js
+++ Event Management System Web/wwwroot/js/Client/viewEvent.js
@@ -3,45 +3,37 @@ const closeViewedEvent = document.getElementById('viewed-event-close-button')
 
 const viewDialogLoad = () => {
     const viewDialogButtons = document.querySelectorAll(".show-button");
-    viewDialogButtons.forEach((viewDialogButton)=> {
-        viewDialogButton.addEventListener("click", (e)=>{
+    //viewDialogButtons.forEach((viewDialogButton)=> {
+    //    viewDialogButton.addEventListener("click", (e)=>{
 
+    //        const viewedEventHeader = e.target.parentElement.parentElement.firstElementChild.firstElementChild;
+    //        const viewedEventBrief = e.target.parentElement.parentElement.firstElementChild.lastElementChild;
+    //        const viewedEventDate = e.target.parentElement.parentElement.parentElement.getAttribute("event-date");
+    //        const viewedEventTime = e.target.parentElement.parentElement.parentElement.getAttribute("event-time");
+    //        const viewedEventImage = e.target.parentElement.parentElement.parentElement.firstElementChild.firstElementChild;
+    //        const viewedEventLocation = e.target.parentElement.parentElement.parentElement.getAttribute("data-viewedEvent-location")
 
-            const viewedEventHeader = e.target.parentElement.parentElement.firstElementChild.firstElementChild;
-            const viewedEventBrief = e.target.parentElement.parentElement.firstElementChild.lastElementChild;
-            const viewedEventDate = e.target.parentElement.parentElement.parentElement.getAttribute("event-date");
-            const viewedEventTime = e.target.parentElement.parentElement.parentElement.getAttribute("event-time");
-            const viewedEventImage = e.target.parentElement.parentElement.parentElement.firstElementChild.firstElementChild;
-            const viewedEventLocation = e.target.parentElement.parentElement.parentElement.getAttribute("data-viewedEvent-location")
+    //        const viewDialogHeader = document.querySelector(".viewed-event-title").lastElementChild;
+    //        const viewDialogBrief = document.querySelector(".viewed-event-brief h3");
+    //        const viewDialogDate = document.querySelector(".viewed-event-date").lastElementChild;
+    //        const viewDialogTime = document.querySelector(".viewed-event-time").lastElementChild;
+    //        const viewDialogImage = document.querySelector(".viewed-event-image");
+    //        const viewDialogLocation = document.querySelector(".viewed-event-location").lastElementChild;
 
+    //        viewDialogHeader.textContent = viewedEventHeader.textContent;
+    //        viewDialogBrief.textContent = viewedEventBrief.textContent;
+    //        viewDialogDate.textContent = viewedEventDate;
+    //        viewDialogTime.textContent = viewedEventTime;
+    //        viewDialogImage.src = viewedEventImage.src;
+    //        viewDialogLocation.href = viewedEventLocation;
+    //        viewDialogLocation.textContent = "";
+    //        /*(viewedEventLocation.split("/")[5].split("+")).join(" ");*/
 
-
-            const viewDialogHeader = document.querySelector(".viewed-event-title").lastElementChild;
-            const viewDialogBrief = document.querySelector(".viewed-event-brief h3");
-            const viewDialogDate = document.querySelector(".viewed-event-date").lastElementChild;
-            const viewDialogTime = document.querySelector(".viewed-event-time").lastElementChild;
-            const viewDialogImage = document.querySelector(".viewed-event-image");
-            const viewDialogLocation = document.querySelector(".viewed-event-location").lastElementChild;
-
-
-            viewDialogHeader.textContent = viewedEventHeader.textContent;
-            viewDialogBrief.textContent = viewedEventBrief.textContent;
-            viewDialogDate.textContent = viewedEventDate;
-            viewDialogTime.textContent = viewedEventTime;
-            viewDialogImage.src = viewedEventImage.src;
-            viewDialogLocation.href = viewedEventLocation;
-            viewDialogLocation.textContent = "";
-            /*(viewedEventLocation.split("/")[5].split("+")).join(" ");*/
-
-
-
-
-
-            viewDialog.showModal()
-            viewDialog.removeAttribute("close")
-            console.log(viewDialogButton);
-        })
-    })
+    //        viewDialog.showModal()
+    //        viewDialog.removeAttribute("close")
+    //        console.log(viewDialogButton);
+    //    })
+    //})
 
     closeViewedEvent.addEventListener("click", ()=>{
         viewDialog.setAttribute("close", "")
